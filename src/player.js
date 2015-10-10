c86b02d037359d16bf1841bb34694a0c7b42c1e2
lhunker <lukas.hunker@gmail.com>
2015-10-10 15:50:43 -0400

    Add skeletons for gameRunner and player

---
+++ src/player.js
@@ -0,0 +1,39 @@
+/**
+ * Created by lhunker on 10/10/15.
+ * A game player class
+ */
+
+/**
+ * Makes a player
+ * @constructor
+ * TODO figure out what data is needed
+ */
+function Player(){
+    //TODO make resource map
+}
+
+/**
+ * Perform move for specific turn
+ * TODO figure out paramters
+ */
+Player.prototype.makeMove = function(){
+  //TODO implement function
+};
+
+/**
+ * Handle adding resources and such per roll
+ * @param roll the number rolled
+ */
+Player.prototype.handleRoll = function (roll){
+    //TODO implement function
+};
+
+/**
+ * Determine the value to the player of a space
+ * @param int the intersection in question
+ */
+function spaceValue(int){
+    //TODO implement function
+}
+
+module.exports = Player;
